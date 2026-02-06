df747b742f97ce44e6d9a7d826ed0724978c2fd6
= <=>
2026-02-06 12:59:50 +0200

    testidatan muuttaminen

--- bookstore/src/main/java/hh/sof3/bookstore/domain/Book.java
+++ bookstore/src/main/java/hh/sof3/bookstore/domain/Book.java
@@ -10,7 +10,7 @@ import jakarta.persistence.Id;
 public class Book {
 
     @Id // PK column
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long bookId;
 
     private String title;
