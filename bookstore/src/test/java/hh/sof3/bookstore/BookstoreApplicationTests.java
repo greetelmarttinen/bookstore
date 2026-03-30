0e5e9b7a7fb98026f2dc01058d248768a7484953
= <=>
2026-03-30 11:11:40 +0300

    smoke test lisätty ja testit meni läpi

--- bookstore/src/test/java/hh/sof3/bookstore/BookstoreApplicationTests.java
+++ bookstore/src/test/java/hh/sof3/bookstore/BookstoreApplicationTests.java
@@ -1,13 +1,28 @@
 package hh.sof3.bookstore;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class BookstoreApplicationTests {
+import hh.sof3.bookstore.web.BookController;
 
+// käynnistää Spring-sovelluksen
+@SpringBootTest
+public class BookstoreApplicationTests {
+
+	// riippuvuuden injektointi
+	@Autowired
+	private BookController bookController;
+	// tällä Spring yrittää luoda BookController-olion ja injektoi sen muuttujaan
+	// bookcontroller
+
+	// "oikea" testi käynnistetään
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		// AsserJ tarkistaa, onko bookcontroller olemassa, eli ei ole null
+		assertThat(bookController).isNotNull();
 	}
 
 }
