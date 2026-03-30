02e08b4583c4f69ed11a1d041e182b7bcba53c78
= <=>
2026-03-30 14:09:28 +0300

    poistettu turha import

--- bookstore/src/test/java/hh/sof3/bookstore/UserRepositoryTest.java
+++ bookstore/src/test/java/hh/sof3/bookstore/UserRepositoryTest.java
@@ -2,8 +2,6 @@ package hh.sof3.bookstore;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Optional;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
